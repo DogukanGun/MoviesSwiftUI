27b48870318dc7b832e657f184aa43fe5a3ad1cc
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2022-07-24 11:33:06 +0300

    Initial Commit

---
+++ MoviesSwiftUI/MoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesSwiftUIApp.swift
+//  MoviesSwiftUI
+//
+//  Created by Dogukan Gundogan on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
